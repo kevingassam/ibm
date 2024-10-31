e049e272f0bfc209a7a859ad333b74284f305ce8
kg <48618359+kevingassam@users.noreply.github.com>
2024-10-31 06:29:09 -0400

    d

--- database/migrations/2024_10_08_012803_create_appartements_table.php
+++ database/migrations/2024_10_08_012803_create_appartements_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('appartements', function (Blueprint $table) {
             $table->id();
             $table->string("nom");
+            $table->string("type")->default("habitation");
             $table->unsignedBigInteger("projet_id");
             $table->timestamps();
 
